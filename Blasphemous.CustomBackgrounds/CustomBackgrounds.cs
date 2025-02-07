f15a3826160afffd2b1456216adc0f2019192841
EltonZhang777 <280903158@qq.com>
2025-02-07 16:45:22 +0800

    Initial commit

---
+++ Blasphemous.CustomBackgrounds/CustomBackgrounds.cs
@@ -0,0 +1,14 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.CustomBackgrounds
+{
+    public class CustomBackgrounds : BlasMod
+    {
+        public CustomBackgrounds() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+        protected override void OnInitialize()
+        {
+            LogError($"{ModInfo.MOD_NAME} has been initialized");
+        }
+    }
+}
---
+++ Blasphemous.CustomBackgrounds/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace Blasphemous.CustomBackgrounds
+{
+    [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+    [BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public static CustomBackgrounds CustomBackgrounds { get; private set; }
+
+        private void Start()
+        {
+            CustomBackgrounds = new CustomBackgrounds();
+        }
+    }
+}
